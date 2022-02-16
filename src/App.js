9f0e3d243c50a684e219fd342865614ca6bce503
Rae <rae.elwell@gmail.com>
2022-02-15 16:34:16 -0800

    resets useState out of dev mode

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
   const [selectedSpell, setSelectedSpell] = useState(null);
   const [currentMonster, setMonster] = useState(null);
   const [playerState, setPlayerState] = useState(null);
-  const [playerGold, setPlayerGold] = useState(300);
+  const [playerGold, setPlayerGold] = useState(0);
   const [actionLogDisplay, setActionLog] = useState([])
   const [portraitIndex, setPortraitIndex] = useState(getRndInteger(0,48))
   const [monsterMultiplier, setMonsterMultiplier] = useState(1);
